13dba3452ae5a7b2052150a93886eeff6e2488fb
Reelman57 <dreese57@gmail.com>
2024-12-16 08:35:44 -0800

    Update IncomingTextHandler.py

--- IncomingTextHandler.py
+++ IncomingTextHandler.py
@@ -1,9 +1,10 @@
+import os
 from twilio.rest import Client
 from flask import Flask, request
 
 # Load credentials from environment variables
-account_sid = "ACe5192182f6433969f03acfbfabeca240"
-auth_token  = "998e399acf35b27c418467e3c6672a4e"
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 twilio_number = "+12086034040"
 
 client = Client(account_sid, auth_token)
@@ -37,8 +38,4 @@ def incoming_sms():
 
     handle_sms(message_body)
 
-    return "SMS received and processed", 200
-
 if __name__ == "__main__":
-    app.run(debug=True)
-    
