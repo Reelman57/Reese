49c163f31477974e09de1fba2f9f8c6844b06003
Reelman57 <dreese57@gmail.com>
2024-12-16 18:35:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,31 +12,31 @@ twilio_number = "+12086034040"
 client = Client(account_sid, auth_token)
 
 @app.route("/sms", methods=['POST'])
-def handle_sms(message_body):
-    first_word = message_body.split()[0]
+# def handle_sms(message_body):
+#     first_word = message_body.split()[0]
 
-    match first_word:
-        case "help":
-            print("Help request received")
-        case "status":
-            print("Status request received")
-        case "Hello":
-            client.messages.create(
-                body=f"From: {message.from_}\nMessage: {message_body}",
-                from_= twilio_number ,
-                to="+15099902828"
-            )
-        case _:
-            client.messages.create(
-                body=f"From: {message.from_}\nMessage: {message_body}",
-                from_= twilio_number ,
-                to="+15099902828"
-            )
+#     match first_word:
+#         case "help":
+#             print("Help request received")
+#         case "status":
+#             print("Status request received")
+#         case "Hello":
+#             client.messages.create(
+#                 body=f"From: {message.from_}\nMessage: {message_body}",
+#                 from_= twilio_number ,
+#                 to="+15099902828"
+#             )
+#         case _:
+#             client.messages.create(
+#                 body=f"From: {message.from_}\nMessage: {message_body}",
+#                 from_= twilio_number ,
+#                 to="+15099902828"
+#             )
 
-def incoming_sms():
-    message_body = request.values.get('Body', None)
+# def incoming_sms():
+#     message_body = request.values.get('Body', None)
 
-    handle_sms(message_body)
+#     handle_sms(message_body)
 
     return "SMS received and processed", 200
 
