a611a56d1fe5a348b5b4cdf87993d7306162630b
Reelman57 <dreese57@gmail.com>
2024-12-17 15:09:55 -0800

    Update SMS_Send.py

--- SMS_Send.py
+++ SMS_Send.py
@@ -114,7 +114,7 @@ for index,row in df_sorted.iterrows():
 
         subject, message = get_message(row)
         # send_email(row['Email'], subject, message)
-        if arg1 
+        if arg1:
           send_texts(row['Phone Number'], message)
         # send_voice(row['Phone Number'], message)
         x+=1
