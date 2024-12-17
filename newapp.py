b595adaf4aff796ebb26a1e4269663dbb4387d92
Reelman57 <dreese57@gmail.com>
2024-12-16 17:37:06 -0800

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,14 +1,17 @@
-from twilio.rest import Client
+import os
 from flask import Flask, request
+from twilio.rest import Client
 
-# Load credentials from environment variables
-account_sid = "ACe5192182f6433969f03acfbfabeca240"
-auth_token  = "998e399acf35b27c418467e3c6672a4e"
-twilio_number = "+12086034040"
+app = Flask(__name__)
+
+# Load your Twilio credentials from environment variables
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+twilio_number = "+12086034040" 
 
 client = Client(account_sid, auth_token)
 
-app = Flask(__name__)
+@app.route("/sms", methods=['POST'])
 
 def handle_sms(message_body):
     first_word = message_body.split()[0]
