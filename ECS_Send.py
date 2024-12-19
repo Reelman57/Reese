76ce535c1b067638097e046eca68db06deaaa13f
Reelman57 <dreese57@gmail.com>
2024-12-19 07:16:08 -0800

    Update ECS_Send.py

--- ECS_Send.py
+++ ECS_Send.py
@@ -61,12 +61,12 @@ get_data()
 for index,row in df_sorted.iterrows():
     if row["Last_Name"] == "Reese" and row["First_Name"] == "Dale":
 
-    print(x, row["Last_Name"], row["First_Name"], row["Phone Number"])
-
-    subject, message = get_message(row)
-    send_email(row['Email'], subject, message)
-    send_texts(row['Phone Number'], message)
-    send_voice(row['Phone Number'], message)
+        print(x, row["Last_Name"], row["First_Name"], row["Phone Number"])
+    
+        subject, message = get_message(row)
+        send_email(row['Email'], subject, message)
+        send_texts(row['Phone Number'], message)
+        send_voice(row['Phone Number'], message)
         
     x+=1
     time.sleep(.05)
