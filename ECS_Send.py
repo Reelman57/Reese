a64e3ca9e8bba5da0520babbe46b737e3b33a600
Reelman57 <dreese57@gmail.com>
2024-12-19 06:21:48 -0800

    Update ECS_Send.py

--- ECS_Send.py
+++ ECS_Send.py
@@ -13,11 +13,12 @@ import pytz
 import re
 import sys
 
-# LIVE Credentials
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-twilio_number = "+12086034040"
-client = Client(account_sid, auth_token)
+from Twilio_Mods import twilio_creds
+from Twilio_Mods import get_send_time
+from Twilio_Mods import send_text
+from Twilio_Mods import get_data
+
+twilio_creds()
 
 if len(sys.argv) > 1:
     arg1 = sys.argv[1]
@@ -31,57 +32,14 @@ else:
 
 x=0
 
-def get_send_time():
-    timezone = pytz.timezone('America/Los_Angeles')
-    now_utc = datetime.now(timezone)
-    send_at = now_utc + timedelta(minutes=5)
-    return send_at
+get_send_time()
 
-def send_texts(text_nbr, message):
-
-    if text_nbr not in sent_texts and not pd.isna(text_nbr):
-        # send_at = get_send_time()
-        message = client.messages.create(
-        body = message,
-        from_='+12086034040',
-        to = text_nbr
-        )
-        # messaging_service_sid = messaging_sid,
-        # send_at=send_at.isoformat(),
-        # schedule_type="fixed"
-        # )
-        sent_texts.add(row["Phone Number"])
-
-def send_voice(to_number, message):
-
-    if to_number not in sent_voice and not pd.isna(to_number):
-
-        call = client.calls.create(
-            twiml = "<Response><Pause length=\"3\"/><Say voice=\"Google.en-US-Standard-J\">" + message + "Goodbye. </Say></Response>",
-            to=to_number,
-            from_="+12086034040"
-        )
-
-        sent_voice.add(row["Phone Number"])
-
-def send_email(to_addr, subject, body):
-
-    if to_addr not in sent_email:
-
-        if isinstance(to_addr, str):
-        
-            msg = MIMEMultipart()
-            msg['From'] = 'eqp77216@gmail.com'
-            msg['To'] = to_addr
-            msg['Subject'] = subject
-            msg.attach(MIMEText(body, 'plain'))
-
-            with smtplib.SMTP('smtp.gmail.com', 587) as smtp:  
-                smtp.starttls()
-                smtp.login('eqp77216@gmail.com', 'ogla wwsg mnqw nmhn') 
-                smtp.sendmail(msg['From'], msg['To'], msg.as_string())
-
-                sent_email.add(row["Email"])
+send_texts(text_nbr, message)
+    sent_texts.add(row["Phone Number"])
+send_voice(to_number, message)
+    sent_voice.add(row["Phone Number"])
+send_email(to_addr, subject, body)
+    sent_email.add(row["Email"])
 
 def get_message(row):
     subject = "Emergency Communications System"
@@ -103,20 +61,9 @@ sent_voice = set()
 # CSV file path
 data_path = "Westmond_Master.csv"
 
-# Read the CSV file
-df = pd.read_csv(data_path)
-df_filtered = df[(df['Age'] > 18)]
-df_sorted = df_filtered.sort_values(by='Last_Name', ascending=True)
-
-with open('DO_NOT_SEND.txt', 'r') as file:
-    sent_texts = set(line.strip() for line in file)
-
-for index,row in df_sorted.iterrows():
+get_data()
 
     print(x, row["Last_Name"], row["First_Name"], row["Phone Number"])
-
-    #if row["Last_Name"] == "Thelin" and row["First_Name"] == "David":
-    if row["Last_Name"] == "Reese":
 
         subject, message = get_message(row)
         send_email(row['Email'], subject, message)
