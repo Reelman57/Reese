bca276177bd87b8bb3100947a74561b0b06f5eab
Reelman57 <dreese57@gmail.com>
2024-12-20 07:57:32 -0800

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ def incoming_sms():
     if first_word == "sms77216" and from_number == '+15099902828':
         subprocess.run(["python", "SMS_Send.py",msg,from_number])
         
+    elif first_word == "min77216" and from_number == '+15099902828' :
+        subprocess.run(["python", "SMS_Ministers.py",msg,from_number])
+
     elif first_word == "cancel-sms" and from_number == '+15099902828' :
         subprocess.run(["python", "SMS_Cancel.py",msg,from_number])
         
