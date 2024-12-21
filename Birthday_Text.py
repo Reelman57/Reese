c29d811be1a250623ef907f45b2ea7a7b01f8746
Reelman57 <dreese57@gmail.com>
2024-12-21 08:41:14 -0800

    Update Birthday_Text.py

--- Birthday_Text.py
+++ Birthday_Text.py
@@ -12,6 +12,7 @@ account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 messaging_sid= os.environ['TWILIO_MSGNG_SID']
 twilio_number = "+12086034040"
+
 Client = Client(account_sid, auth_token)
 
 def get_send_time():
