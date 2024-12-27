5bb3217aa24fa961b25b9123b01229b2cab97b06
Reelman57 <dreese57@gmail.com>
2024-12-27 15:17:39 -0800

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def send_text(text_nbr, message, now):
             message = client.messages.create(
                 body=message,
                 from_=twilio_number,
-                to=f"+1{text_nbr}",
+                to=text_nbr,
                 messaging_service_sid=messaging_sid,
                 send_at=send_at,
                 schedule_type=schedule_type
