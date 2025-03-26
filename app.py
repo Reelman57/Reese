9706de437c1dac61c67d004b88747ed7a77fd22b
Reelman57 <dreese57@gmail.com>
2025-03-26 09:30:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -367,7 +367,7 @@ def incoming_sms():
         confirm_send()
         return "Ministering district messages sent.", 200
 # --------------------------------------------------------------------------
-    elif first_word == "minall77216" and from_number in authorized_list:
+    elif first_word == "xminall77216" and from_number in authorized_list:
        
         try:
             df = pd.read_csv(data_file)
@@ -417,6 +417,66 @@ def incoming_sms():
         
                     print(minister_last," - ", minister_phone,"  " ,minister_email,msg)
                     send_text(text_nbr, msg, False)
+                    #send_email(minister_email,subj,msg)
+
+        confirm_send()
+        return "Ministering district messages sent.", 200
+# --------------------------------------------------------------------------
+    elif first_word == "minall77216" and from_number in authorized_list:
+       
+        try:
+            df = pd.read_csv(data_file)
+        except FileNotFoundError:
+            return "Error: File not found.", 500
+        except Exception as e:
+            return f"Error reading data file: {e}", 500
+        
+        for r in range(1, 3): 
+            
+            ministerr = f"Minister{r}"
+            ministerr_phone = f"Minister{r}_Phone"
+            ministerr_email = f"Minister{r}_Email"
+        
+            df_filtered = df[df[ministerr].notnull()]
+            df_filtered = df_filtered[df_filtered['Age'] > 17]
+            df_filtered[ministerr] = df_filtered[ministerr].fillna('')
+
+            if ministerr==Minister1:
+                Comp=f"Minister2"
+            else:
+                Comp=f"Minister1"
+            
+            try:
+                df_filtered[['Minister_Last', 'Minister_First']] = df_filtered[ministerr].str.split(',', expand=True) 
+            except AttributeError as e:
+                print(f"Error splitting {ministerr} for potential missing or invalid data: {e}")
+                continue  # Skip to the next iteration of the outer for loop
+        
+            grouped_df = df_filtered.groupby(["Minister_Last", "Minister_First", ministerr_phone, ministerr_email])
+        
+            for (minister_last, minister_first, minister_phone, minister_email), group in grouped_df:
+          
+                text_nbr = minister_phone
+                subj="Your Ministering Families"
+        
+                if r > 2:
+                    Bro_Sis = "Sister"
+                else:
+                    Bro_Sis = "Brother"
+                    
+                msg = f"{Bro_Sis} {minister_last}, \n"
+                msg += f"{msg_in} \n\n"
+                msg += f"{minister_first.strip()}, just tap on the phone numbers below for options on ways to message them.\n\n"
+        
+                if not group.empty:
+                    for index, row in group.iterrows():
+                        msg += f"{row['Name']}"
+                        if not pd.isna(row['Phone Number']):
+                            msg += f"  - {row['Phone Number']}"
+                        msg += "\n"
+        
+                    print(Comp," ",minister_last," - ", minister_phone,"  " ,minister_email,msg)
+                    #send_text(text_nbr, msg, False)
                     #send_email(minister_email,subj,msg)
 
         confirm_send()
