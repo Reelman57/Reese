4208570959dbec9a6cc2b7041e8c3f857e557541
Reelman57 <dreese57@gmail.com>
2025-05-20 08:56:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -519,8 +519,7 @@ def incoming_sms():
             msg = f"Brother {last_name}, \n\n"
             msg += msg_in
             print(msg) 
-            if data['Phone Number'] == "5099900248":
-                send_text(data['Phone Number'], msg, False)
+            send_text(data['Phone Number'], msg, False)
 
         confirm_send()
         return "Messages sent successfully.", 200
