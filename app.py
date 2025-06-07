82884201f3675cb4c1d6e53f2838fc622c312d5c
Reelman57 <dreese57@gmail.com>
2025-06-07 10:00:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,9 @@ def get_send_time():
     return send_at.isoformat()
 # --------------------------------------------------------------------------
 def send_text(text_nbr, message, now):
+    global x 
+    global sent_texts 
+
     if text_nbr not in sent_texts and not pd.isna(text_nbr):
         if not now:
             send_at = get_send_time()
