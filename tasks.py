726f00dcec408ead4279583112c6f656daa118f3
Reelman57 <dreese57@gmail.com>
2025-06-24 10:36:45 -0700

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -5,6 +5,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from xml.sax.saxutils import escape
+from concurrent.futures import ThreadPoolExecutor
 
 import pandas as pd
 from twilio.rest import Client
