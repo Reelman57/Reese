364c3fb0298a5c2b2f1422f53748d3257d21bacc
Reelman57 <dreese57@gmail.com>
2025-06-24 08:19:19 -0700

    Update worker.py

--- worker.py
+++ worker.py
@@ -3,11 +3,10 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 
+from app import send_emails, send_voice
+
 listen = ['high', 'default', 'low']
-
-# Get the Redis URL from the Heroku environment variable
 redis_url = os.getenv('REDISCLOUD_URL', os.getenv('REDIS_URL', 'redis://localhost:6379'))
-
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
