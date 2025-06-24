714545b62fdb2aa55750fb68dab3debbefbfc13f
Reelman57 <dreese57@gmail.com>
2025-06-24 09:38:12 -0700

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,15 +1,15 @@
-# worker.py
 import os
 import redis
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue 
 
 from app import send_emails, send_voice
 
 listen = ['high', 'default', 'low']
+
 redis_url = os.getenv('REDISCLOUD_URL', os.getenv('REDIS_URL', 'redis://localhost:6379'))
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+    queues = [Queue(name, connection=conn) for name in listen]
+    worker = Worker(queues, connection=conn)
+    worker.work()
