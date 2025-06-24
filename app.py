50fdbf56087e8f8077ff7134c1751646ef4d9637
Reelman57 <dreese57@gmail.com>
2025-06-24 07:43:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ import sys
 import time
 import uuid
 import smtplib
+
+import redis
+from rq import Queue # In app.py
+redis_url = os.getenv('REDISCLOUD_URL', os.getenv('REDIS_URL', 'redis://localhost:6379'))
+redis_conn = redis.from_url(redis_url)
+q = Queue(connection=redis_conn)
+
+# In worker.py
+conn = redis.from_url(redis_url)
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from concurrent.futures import ThreadPoolExecutor
