f954e5607145aa53e7e272340a73be618d6aee06
Reelman57 <dreese57@gmail.com>
2025-06-25 08:32:47 -0700

    Update Birthday_Text.py

--- Birthday_Text.py
+++ Birthday_Text.py
@@ -51,6 +51,8 @@ def get_send_time():
 # --------------------------------------------------------------------------
     
 def send_text(text_nbr, message):
+    print(f"Attempting to schedule for {text_nbr}. send_at value: {send_at}, type: {type(send_at)}")
+
     if text_nbr not in sent_texts and not pd.isna(text_nbr):
         send_at = get_send_time()
         message = client.messages.create(
